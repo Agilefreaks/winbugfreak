f57e498e74a23ebb3a0eaac49f4403a77fe81abe
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-07-05 10:13:11 +0300

    modified output path for BugFreak build

--- Code/BugFreak.Core/BugFreak.Tests/Properties/AssemblyInfo.cs
+++ Code/BugFreak.Core/BugFreak.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AgileErrorReporting.Tests")]
+[assembly: AssemblyTitle("BugFreak.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AgileErrorReporting.Tests")]
+[assembly: AssemblyProduct("BugFreak.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Code/BugFreak.Core/BugFreak/Properties/AssemblyInfo.cs
+++ Code/BugFreak.Core/BugFreak/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AgileBug")]
+[assembly: AssemblyTitle("BugFreak")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AgileBug")]
+[assembly: AssemblyProduct("BugFreak")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
