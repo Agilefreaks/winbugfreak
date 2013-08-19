5b98cf59e162eaa84bff9ced861c28ce3936a63d
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-08-19 17:01:00 +0300

    added mvc3 sample app project

---
+++ Code/Samples/MVC3.Sample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MVC3.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Boom()
+        {
+            throw new Exception("Boom!");
+        }
+    }
+}
