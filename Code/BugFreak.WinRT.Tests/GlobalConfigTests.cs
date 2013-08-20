a70513a460257cd3a2b4b0f122a80c1576e8108d
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-08-20 15:06:13 +0300

    added WinRT project

---
+++ Code/BugFreak.WinRT.Tests/GlobalConfigTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using BugFreak.Components;
+using NUnit.Framework;
+
+namespace BugFreak.WinRT.Tests
+{
+    [TestFixture]
+    public class GlobalConfigTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            GlobalConfig.Settings.ApiKey = "apiKey";
+            GlobalConfig.Settings.Token = "token";
+        }
+
+        [Test]
+        public void GetServicesIErrorReportStorage_Always_ReturnsRemoteErrorReportStorage()
+        {
+            ReportingService.Init();
+            
+            var storage = GlobalConfig.ServiceLocator.GetServices<IErrorReportStorage>();
+
+            Assert.IsTrue(storage.Any(s => s is RemoteErrorReportStorage));
+        }
+    }
+}
