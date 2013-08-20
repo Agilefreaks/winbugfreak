6d9df657b297c4e078cd225fb84ece32500c11dd
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-08-20 15:08:13 +0300

    added WinRT sample project

---
+++ Code/Samples/WinRT.Sample/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace WinRT.Sample
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            CallToSetStackTrace();
+        }
+
+        private void CallToSetStackTrace()
+        {
+            Boom();
+        }
+
+        private void Boom()
+        {
+            throw new Exception("Boom!");
+        }
+    }
+}
