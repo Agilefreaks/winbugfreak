ec82916b98355a87c24b9a4295c5c6a7fd37a5cf
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-08-20 15:47:11 +0300

    added nuspec and build compilation symbols

--- Code/BugFreak.WinRT/Properties/AssemblyInfo.cs
+++ Code/BugFreak.WinRT/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BugFreak.WinRT")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("BugFreak for WinRT. Easily track your applications errors. More info at https://bugfreak.co")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AgileFreaks")]
 [assembly: AssemblyProduct("BugFreak.WinRT")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
