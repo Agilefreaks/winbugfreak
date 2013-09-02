636e34078f95bec5fabb5f8c9ff157dfa5f5ab48
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-09-02 11:38:43 +0300

    set handled to true to prevent interrupting reporting process and on complete shut down app

--- Code/BugFreak.WinRT/BugFreak.cs
+++ Code/BugFreak.WinRT/BugFreak.cs
@@ -1,10 +1,13 @@
 ﻿namespace BugFreak
 {
+    using System;
     using global::BugFreak.WinRT.Components;
     using Windows.UI.Xaml;
     
     public static class BugFreak
     {
+        private static Application _app;
+
         public static void Hook(string apiKey, string token, Application app)
         {
             GlobalConfig.ApiKey = apiKey;
@@ -12,8 +15,9 @@
 
             GlobalConfig.ErrorDataProviders.Add(new PackageInfoProvier());
             ReportingService.Init();
-            
-            app.UnhandledException += OnException;
+
+            _app = app;
+            _app.UnhandledException += OnException;
         }
 
         private static void OnException(object sender, UnhandledExceptionEventArgs eventArgs)
@@ -21,7 +25,14 @@
             var exception = eventArgs.Exception;
             exception.Data.Add("Message", eventArgs.Message);
 
-            ReportingService.Instance.BeginReport(exception);
+            // set as handled to allow reporting error and on complete shut down app
+            ReportingService.Instance.BeginReport(exception, OnReportCompleted);
+            eventArgs.Handled = true;
+        }
+
+        private static void OnReportCompleted(Exception exc, bool reported)
+        {
+            _app.Exit();
         }
     }
 }
